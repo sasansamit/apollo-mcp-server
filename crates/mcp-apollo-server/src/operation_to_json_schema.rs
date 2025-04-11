7280f3a33f006c646fdb53312c53c3855c2acca9
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2025-04-11 14:48:59 -0600

    update to return more information

--- crates/mcp-apollo-server/src/operation_to_json_schema.rs
+++ crates/mcp-apollo-server/src/operation_to_json_schema.rs
@@ -1,5 +1,17 @@
-use apollo_compiler::{ast::{Definition, Type}, parser::Parser};
-use rmcp::schemars::schema::{ArrayValidation, InstanceType, ObjectValidation, RootSchema, Schema, SchemaObject, SingleOrVec, SubschemaValidation};
+use apollo_compiler::{
+    ast::{Definition, Type},
+    parser::Parser,
+};
+use rmcp::schemars::schema::{
+    ArrayValidation, InstanceType, ObjectValidation, RootSchema, Schema, SchemaObject, SingleOrVec,
+    SubschemaValidation,
+};
+
+pub struct ToolDefinition {
+    pub name: String,
+    pub description: String,
+    pub schema: RootSchema,
+}
 
 /**
  * TODO
@@ -7,23 +19,31 @@ use rmcp::schemars::schema::{ArrayValidation, InstanceType, ObjectValidation, Ro
  * - support for custom scalars
  * - error handling
  */
-pub fn operation_to_json_schema(uri: &str, source_text: &str) -> RootSchema {
-    let document = Parser::new().parse_ast(source_text, uri).expect("failed to parse operation");
-    let operation_variables = document.definitions.iter().filter_map(|def| match def {
-        Definition::OperationDefinition(operation_def) => Some(operation_def.variables.clone()),
+pub fn operation_to_json_schema(uri: &str, source_text: &str) -> ToolDefinition {
+    let document = Parser::new()
+        .parse_ast(source_text, uri)
+        .expect("failed to parse operation");
+    let operation_defs = document.definitions.iter().filter_map(|def| match def {
+        Definition::OperationDefinition(operation_def) => Some(operation_def),
         Definition::FragmentDefinition(_) => None,
         _ => panic!("Schema definitions were passed in, only operations and fragments are allowed"),
     });
 
-    let operation_count = operation_variables.clone().count();
-    assert!(operation_count <= 1, "too many operations in document: {operation_count}");
+    let operation_count = operation_defs.clone().count();
+    assert!(
+        operation_count <= 1,
+        "too many operations in document: {operation_count}"
+    );
 
     let mut obj = ObjectValidation::default();
 
-    operation_variables
+    let operation = operation_defs
         .clone()
         .nth(0)
-        .expect("no operations in document")
+        .expect("no operations in document");
+
+    operation
+        .variables
         .iter()
         .for_each(|variable| {
             let variable_name = variable.name.to_string();
@@ -34,76 +54,76 @@ pub fn operation_to_json_schema(uri: &str, source_text: &str) -> RootSchema {
             }
         });
 
-    RootSchema {
-        schema: SchemaObject {
-            instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::Object))),
-            object: Some(Box::new(obj)),
-            ..Default::default()
-        },
-        ..Default::default()
-    }
+        ToolDefinition {
+            name: operation.name.clone().expect("Operations require names").to_string(),
+            description: "".to_string(),
+            schema: RootSchema {
+                schema: SchemaObject {
+                    instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::Object))),
+                    object: Some(Box::new(obj)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        }
 }
 
 fn type_to_schema(variable_type: &Type) -> Schema {
     match variable_type {
-        Type::NonNullNamed(named) | Type::Named(named) => {
-            Schema::Object(SchemaObject {
-                instance_type: Some(SingleOrVec::Single(match named.as_str() {
-                    "String" | "ID" => Box::new(InstanceType::String),
-                    "Int" | "Float" => Box::new(InstanceType::Number),
-                    "Boolean" => Box::new(InstanceType::Boolean),
-                    _ => panic!("Only build in scalars are currently supported"),
-                })),
-                ..Default::default()
-            })
-        },
-        Type::NonNullList(list_type) | Type::List(list_type) => {
-            Schema::Object(SchemaObject {
-                instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::Array))),
-                subschemas: if list_type.is_non_null() { 
-                    None 
-                } else {
-                    Some(Box::new(SubschemaValidation {
-                        one_of: Some(vec![
-                            type_to_schema(list_type),
-                            Schema::Object(SchemaObject {
-                                instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::Null))),
-                                ..Default::default()
-                            }),
-                        ]),
-                        ..Default::default()
-                    }))
-                },
-                array: if list_type.is_non_null() {
-                    Some(Box::new(ArrayValidation {
-                        items: Some(SingleOrVec::Single(Box::new(type_to_schema(list_type)))),
-                        ..Default::default()
-                    }))
-                } else {
-                    None 
-                },
-                ..Default::default()
-            })
-        }
+        Type::NonNullNamed(named) | Type::Named(named) => Schema::Object(SchemaObject {
+            instance_type: Some(SingleOrVec::Single(match named.as_str() {
+                "String" | "ID" => Box::new(InstanceType::String),
+                "Int" | "Float" => Box::new(InstanceType::Number),
+                "Boolean" => Box::new(InstanceType::Boolean),
+                _ => panic!("Only build in scalars are currently supported"),
+            })),
+            ..Default::default()
+        }),
+        Type::NonNullList(list_type) | Type::List(list_type) => Schema::Object(SchemaObject {
+            instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::Array))),
+            subschemas: if list_type.is_non_null() {
+                None
+            } else {
+                Some(Box::new(SubschemaValidation {
+                    one_of: Some(vec![
+                        type_to_schema(list_type),
+                        Schema::Object(SchemaObject {
+                            instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::Null))),
+                            ..Default::default()
+                        }),
+                    ]),
+                    ..Default::default()
+                }))
+            },
+            array: if list_type.is_non_null() {
+                Some(Box::new(ArrayValidation {
+                    items: Some(SingleOrVec::Single(Box::new(type_to_schema(list_type)))),
+                    ..Default::default()
+                }))
+            } else {
+                None
+            },
+            ..Default::default()
+        }),
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::operation_to_json_schema::{operation_to_json_schema, ToolDefinition};
     use rmcp::serde_json::json;
-    use crate::operation_to_json_schema::operation_to_json_schema;
 
     #[test]
     fn no_variables() {
-        let root_schema = operation_to_json_schema("operation.graphql", "query { id }");
-        assert_eq!(json!(root_schema), json!({"type": "object"}))
+        let ToolDefinition { name: _name, description: _desciption, schema } = operation_to_json_schema("operation.graphql", "query { id }");
+        assert_eq!(json!(schema), json!({"type": "object"}))
     }
 
     #[test]
     fn nullable_named_type() {
-        let root_schema = operation_to_json_schema("operation.graphql", "query($id: ID) { id }");
+        let ToolDefinition { name: _name, description: _desciption, schema } = operation_to_json_schema("operation.graphql", "query($id: ID) { id }");
         assert_eq!(
-            json!(root_schema),
+            json!(schema),
             json!({
                 "type": "object",
                 "properties": { "id": {"type": "string"} }
@@ -113,9 +133,9 @@ mod tests {
 
     #[test]
     fn non_nullable_named_type() {
-        let root_schema = operation_to_json_schema("operation.graphql", "query($id: ID!) { id }");
+        let ToolDefinition { name: _name, description: _desciption, schema } = operation_to_json_schema("operation.graphql", "query($id: ID!) { id }");
         assert_eq!(
-            json!(root_schema),
+            json!(schema),
             json!({
                 "type": "object",
                 "properties": { "id": {"type": "string"} },
@@ -126,9 +146,9 @@ mod tests {
 
     #[test]
     fn non_nullable_list_of_nullable_named_type() {
-        let root_schema = operation_to_json_schema("operation.graphql", "query($id: [ID]!) { id }");
+        let ToolDefinition { name: _name, description: _desciption, schema } = operation_to_json_schema("operation.graphql", "query($id: [ID]!) { id }");
         assert_eq!(
-            json!(root_schema),
+            json!(schema),
             json!({
                 "type": "object",
                 "properties": {
@@ -144,9 +164,10 @@ mod tests {
 
     #[test]
     fn non_nullable_list_of_non_nullable_named_type() {
-        let root_schema = operation_to_json_schema("operation.graphql", "query($id: [ID!]!) { id }");
+        let ToolDefinition { name: _name, description: _desciption, schema } =
+            operation_to_json_schema("operation.graphql", "query($id: [ID!]!) { id }");
         assert_eq!(
-            json!(root_schema),
+            json!(schema),
             json!({
                 "type": "object",
                 "properties": { "id": {"type": "array", "items": { "type": "string" }} },
@@ -157,9 +178,9 @@ mod tests {
 
     #[test]
     fn nullable_list_of_nullable_named_type() {
-        let root_schema = operation_to_json_schema("operation.graphql", "query($id: [ID]) { id }");
+        let ToolDefinition { name: _name, description: _desciption, schema } = operation_to_json_schema("operation.graphql", "query($id: [ID]) { id }");
         assert_eq!(
-            json!(root_schema),
+            json!(schema),
             json!({
                 "type": "object",
                 "properties": {
@@ -174,9 +195,9 @@ mod tests {
 
     #[test]
     fn nullable_list_of_non_nullable_named_type() {
-        let root_schema = operation_to_json_schema("operation.graphql", "query($id: [ID!]) { id }");
+        let ToolDefinition { name: _name, description: _desciption, schema } = operation_to_json_schema("operation.graphql", "query($id: [ID!]) { id }");
         assert_eq!(
-            json!(root_schema),
+            json!(schema),
             json!({
                 "type": "object",
                 "properties": { "id": {"type": "array", "items": { "type": "string" }} },
@@ -186,9 +207,10 @@ mod tests {
 
     #[test]
     fn nullable_list_of_nullable_lists_of_nullable_named_types() {
-        let root_schema = operation_to_json_schema("operation.graphql", "query($id: [[ID]]) { id }");
+        let ToolDefinition { name: _name, description: _desciption, schema } =
+            operation_to_json_schema("operation.graphql", "query($id: [[ID]]) { id }");
         assert_eq!(
-            json!(root_schema),
+            json!(schema),
             json!({
                 "type": "object",
                 "properties": {
