c2c4183a5dd72ca3d7c030b884900c0e23bca4e6
Daniel Dorsey <daniel.dorsey@apollographql.com>
2025-07-07 15:19:58 -0500

    Fetch markdown file for connectors

--- crates/apollo-mcp-server/src/connectors.rs
+++ crates/apollo-mcp-server/src/connectors.rs
@@ -1,12 +1,13 @@
 use crate::errors::McpError;
 use crate::schema_from_type;
-use rmcp::model::{CallToolResult, Content, Tool};
+// use reqwest::header::HeaderMap;
+use rmcp::model::{CallToolResult, Content, ErrorCode, Tool};
 use rmcp::schemars::JsonSchema;
 use rmcp::serde_json::Value;
 use rmcp::{schemars, serde_json};
 use serde::{Deserialize, Serialize};
 
-pub(crate) const CONNECTORS_TOOL_NAME: &str = "connectors";
+pub(crate) const CONNECTORS_TOOL_NAME: &str = "connectors-spec";
 
 #[derive(Clone)]
 pub struct Connectors {
@@ -25,11 +26,12 @@ fn default_input() -> String {
 }
 
 impl Connectors {
+
     pub fn new() -> Self {
         Self {
             tool: Tool::new(
                 CONNECTORS_TOOL_NAME,
-                "Get the connectors specification markdown",
+                "This tool fetches the specification which describes how to use Apollo Connectors in a graphql schema to send an HTTP request. A user may refer to an Apollo Connector as 'Apollo Connector', 'REST Connector', or even just 'Connector'. Treat these all as synonyms for the same thing. If a user is trying to write a Connector, you should use this specification as a guide.",
                 schema_from_type!(Input),
             ),
         }
@@ -37,10 +39,35 @@ impl Connectors {
 
     pub async fn execute(&self) -> Result<CallToolResult, McpError> {
 
-        // TODO: Fetch the markdown file here (or in it's own function for testability purposes?)
+        let content = reqwest::Client::new()
+            .get(r"https://raw.githubusercontent.com/apollographql/router/refs/heads/am/connectorsllmmd/connectors-llm/connector-llm.md")
+            .send()
+            .await
+            .map_err(|err| {
+                McpError::new(
+                    ErrorCode::INTERNAL_ERROR,
+                    format!("Failed to query for connectors markdown file: {err}"),
+                    None,
+                )
+            })
+            .map( async |response| {
+                response.text().await
+            });
+
+        let result = match content {
+            Ok(fut) => fut.await,
+            Err(_) => todo!(),
+        };
+
+        let result = match result {
+            Ok(value) => value,
+            Err(_) => todo!(),
+        };
+
+        println!("{:?}", result);
         
         Ok(CallToolResult {
-            content: vec![Content::text("Hello world")],
+            content: vec![Content::text(result)],
             is_error: None,
         })
     }
