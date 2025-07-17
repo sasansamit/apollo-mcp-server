8be60f2129c192662fbbff80ae8f41be01065f8c
Nicholas Cioli <nicholas.cioli@apollographql.com>
2025-07-17 15:40:46 -0400

    prepare for release candidate 3

--- scripts/nix/install.sh
+++ scripts/nix/install.sh
@@ -14,7 +14,7 @@ BINARY_DOWNLOAD_PREFIX="${APOLLO_MCP_SERVER_BINARY_DOWNLOAD_PREFIX:="https://git
 
 # Apollo MCP Server version defined in apollo-mcp-server's Cargo.toml
 # Note: Change this line manually during the release steps.
-PACKAGE_VERSION="v0.6.0-rc.2"
+PACKAGE_VERSION="v0.6.0-rc.3"
 
 download_binary_and_run_installer() {
     downloader --check
